73ca370214d38344a129daa343dab55f07d23e56
A Raza Khan <pathantcs@gmail.com>
2024-01-08 14:53:49 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Welcome from Jenkins!!");
+		System.out.println(LocalDateTime.now());
+	}
+}
