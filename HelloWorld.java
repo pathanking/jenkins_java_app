b8a904778820ec50b10ee464fda415b01cc5e3de
A Raza Khan <pathantcs@gmail.com>
2024-01-19 15:02:32 +0530

    minor updates

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
 import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Welcome from Jenkins!!");
 		System.out.println(LocalDateTime.now());
-		System.out.println("Welcome Again!!");
+		
+		System.out.println(LocalDate.now());
+		System.out.println(LocalTime.now());
 	}
 }
